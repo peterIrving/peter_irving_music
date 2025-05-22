68d7471cddb2199247f11891cf999be936000e46
Peter Irving <pirving1@fmi.com>
2025-05-21 20:34:40 -0500

    trying more analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,16 +14,13 @@ import umami from "@yeskunall/astro-umami";
 export default defineConfig({
 	site: "https://www.peterirving.live",
 	base: "/",
-	integrations: [
-		// example auto import component into blog post mdx files
+	integrations: [// example auto import component into blog post mdx files
 		AutoImport({
 			imports: [
 				// https://github.com/delucis/astro-auto-import
 				"@components/Admonition/Admonition.astro",
 			],
-		}),
-		mdx(),
-		icon({
+		}), mdx(), icon({
 			// I include only the icons I use. This is because if you use SSR, ALL icons will be included (no bueno)
 			// https://www.astroicon.dev/reference/configuration#include
 			include: {
@@ -43,22 +40,17 @@ export default defineConfig({
 				],
 				gridicons: ["*"],
 			},
-		}),
-		sitemap(),
-		vue(),
-		compress({
+		}), sitemap(), vue(), compress({
 			HTML: true,
 			JavaScript: true,
 			CSS: true,
 			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
 			SVG: false, // astro-icon handles this
-		}),
-		umami({
+		}), umami({
 			websiteId: "e0965b9b-2d3b-4d00-8339-04bf810d5024",
 			src: "https://cloud.umami.is/script.js",
 			domains: ["peterirving.live", "www.peterirving.live"],
-		}),
-	],
+		}),],
 	vite: {
 		plugins: [tailwindcss()],
 		// stop inlining short scripts to fix issues with ClientRouter: https://github.com/withastro/astro/issues/12804
